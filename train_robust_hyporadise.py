6b245b6e916d40ac16597d1e144249e74e3ce05a
gipder <rimhsak@naver.com>
2024-11-14 00:20:52 -0500

    working with llama2

--- train_robust_hyporadise.py
+++ train_robust_hyporadise.py
@@ -26,9 +26,6 @@ class MaskedTrainer(Trainer):
 
         return (loss, outputs) if return_outputs else loss
 
-## 1. 모델과 토크나이저 로드
-#model_name = "huggingface/llama-3"  # 실제 LLaMA-3 분류 모델로 대체해야 함
-#tokenizer = AutoTokenizer.from_pretrained(model_name)
 # 1. 모델과 토크나이저 준비
 # 일단은 Llama-2를 사용 왜냐하면 학습 DB에 Tokenizer가 Llama-2로 되어 있기 때문
 model_name = "meta-llama/Llama-2-7b-hf"
@@ -36,7 +33,7 @@ tokenizer = AutoTokenizer.from_pretrained(model_name)
 tokenizer.pad_token = tokenizer.eos_token
 
 model = LlamaForCausalLM.from_pretrained(model_name,
-                                         torch_dtype=torch.bfloat16,
+                                         torch_dtype=torch.float32,
                                          device_map="auto")
 
 model.config.pad_token_id = model.config.eos_token_id
@@ -53,22 +50,17 @@ lora_config = LoraConfig(
 model = get_peft_model(model, lora_config)
 
 # 2. 데이터셋 로드
-data = torch.load("data/clean.pt")
+test_data = torch.load("data/clean.pt")
+data = torch.load("/home/sskim/work/Robust-HyPoradise/train_noizeus.pt")
 dataset = Dataset.from_list(data)
+test_dataset = Dataset.from_list(test_data)
 
 # 데이터 샘플링 (학습 시간을 줄이기 위해 작은 데이터셋으로 작업)
-#small_train_dataset = dataset.shuffle(seed=42).select(range(1000))
-#small_test_dataset = dataset.shuffle(seed=42).select(range(200))
 small_train_dataset = dataset.shuffle(seed=42)
-small_test_dataset = dataset.shuffle(seed=42)
+small_test_dataset = test_dataset.shuffle(seed=42)
 
 # 3. 데이터 전처리 함수 정의
 def preprocess_data(batch):
-    # calculate attention mask
-    # batch["attention_mask"] = [1] * len(batch["input_ids"])
-    # calculate labels mask
-    # batch["labels_mask"] = [0 if label == -1 else 1 for label in batch["labels"]]
-    # return inputs
     # Attention mask 생성
     attention_mask = [[1] * len(input_ids) for input_ids in batch["input_ids"]]
     labels = [[-100 if label == -1 else label for label in labels] for labels in batch["labels"]]
@@ -77,66 +69,56 @@ def preprocess_data(batch):
         "input_ids": batch["input_ids"],
         "attention_mask": attention_mask,
         "labels": labels,
-        #"labels_mask": labels_mask,
     }
 
-#print(f"{inputs=}")
 # 데이터셋 전처리 적용
 train_dataset = small_train_dataset.map(preprocess_data, batched=True)
 test_dataset = small_test_dataset.map(preprocess_data, batched=True)
 
 # torch 텐서 형식으로 변환
-#train_dataset.set_format(type="torch", columns=["input_ids", "attention_mask", "labels", "labels_mask"])
-#train_dataset.set_format(type="torch", columns=["input_ids", "attention_mask", "labels", "labels_mask"])
-#test_dataset.set_format(type="torch", columns=["input_ids", "attention_mask", "labels", "labels_mask"])
 train_dataset.set_format(type="torch", columns=["input_ids", "attention_mask", "labels"])
 test_dataset.set_format(type="torch", columns=["input_ids", "attention_mask", "labels"])
 
-def collate_fn(batch):
+# 데이터 콜레이터 정의
+def collate_fn(batch, max_length=256):
     # get the max length of the batch
     max_input_length = max(len(item["input_ids"]) for item in batch)
     max_label_length = max(len(item["labels"]) for item in batch)
+    if max_input_length > max_length:
+        max_input_length = max_length
 
     # padding with tensor in torch
     input_ids_type = batch[0]["input_ids"].dtype
     attention_mask_type = batch[0]["attention_mask"].dtype
     labels_type = batch[0]["labels"].dtype
-    #labels_mask_type = batch[0]["labels_mask"].dtype
+
     padded_input_ids = torch.zeros((len(batch), max_input_length), dtype=input_ids_type)
     # fill with -100 for labels
     padded_labels = torch.full((len(batch), max_label_length), -100, dtype=labels_type)
-    #padded_labels = torch.zeros((len(batch), max_label_length), dtype=labels_type)
     padded_attention_mask = torch.zeros((len(batch), max_input_length), dtype=attention_mask_type)
 
     for idx in range(len(batch)):
-        padded_input_ids[idx, :len(batch[idx]["input_ids"])] = batch[idx]["input_ids"]
-        padded_labels[idx, :len(batch[idx]["labels"])] = batch[idx]["labels"]
-        padded_attention_mask[idx, :len(batch[idx]["attention_mask"])] = batch[idx]["attention_mask"]
-        #padded_labels_mask[idx, :len(batch[idx]["labels_mask"])] = batch[idx]["labels_mask"]
+        input_length = min(len(batch[idx]["input_ids"]), max_input_length)
+        label_length = min(len(batch[idx]["labels"]), max_label_length)
+
+        padded_input_ids[idx, :input_length] = batch[idx]["input_ids"][:input_length]
+        padded_labels[idx, :label_length] = batch[idx]["labels"][:label_length]
+        padded_attention_mask[idx, :input_length] = batch[idx]["attention_mask"][:input_length]
 
     return {
         "input_ids": padded_input_ids.clone().detach(),
         "labels": padded_labels.clone().detach(),
         "attention_mask": padded_attention_mask.clone().detach()
-        #"labels_mask": padded_labels_mask.clone().detach(),
     }
 
-# 데이터 콜레이터 정의
-#for item in train_dataset:
-#    print(f"{item=}")
-#    break
-#data_loader = DataLoader(train_dataset, batch_size=4, shuffle=True, collate_fn=collate_fn)
-#for batch in data_loader:
-#    print(tokenizer.decode(batch["input_ids"][0]))
-#    break
+# Testing the function collate_fn
 
-# 4. 데이터 콜레이터 정의
 # 4. 훈련 파라미터 설정
 training_args = TrainingArguments(
     output_dir="./llama2-lora-finetuned",
-    per_device_train_batch_size=4,
-    gradient_accumulation_steps=2,
-    num_train_epochs=30,
+    per_device_train_batch_size=5,
+    gradient_accumulation_steps=4,
+    num_train_epochs=5,
     logging_dir="./logs",
     logging_steps=10,
     learning_rate=5e-4,
@@ -151,7 +133,8 @@ trainer = Trainer(
     data_collator=collate_fn,
 )
 
-for item in train_dataset:
+# 시작전에 Testing 해보기
+for item in test_dataset:
     prompt_mask = (item['labels'] == -100)
     # get prompt with prompt mask
     prompt = item['input_ids'] * prompt_mask
@@ -176,7 +159,7 @@ trainer.train()
 
 print("After training")
 print("=" * 10)
-for item in train_dataset:
+for item in test_dataset:
     prompt_mask = (item['labels'] == -100)
     # get prompt with prompt mask
     prompt = item['input_ids'] * prompt_mask
@@ -193,26 +176,5 @@ for item in train_dataset:
     input_text = tokenizer.decode(inputs['input_ids'][0, :prompt_length], skip_special_tokens=True)
     output = output[0, prompt_length:]
     print("*** Input text: \n", input_text)
-    print("Generated output: ", tokenizer.decode(output, skip_special_tokens=True))
+    print("*** Generated output: ", tokenizer.decode(output, skip_special_tokens=True))
     break
-
-
-# 7. 모델 평가
-#eval_results = trainer.evaluate()
-#print(f"Evaluation results: {eval_results}")
-
-# 1. 테스트 데이터셋에 대한 예측 수행
-#predictions = trainer.predict(test_dataset)
-#print("predictions.predictions: ", predictions.predictions)
-#pred_labels = predictions.predictions[1].argmax(axis=1)  # 예측된 레이블
-#true_labels = predictions.label_ids  # 실제 레이블
-#true_labels = test_dataset['labels']
-
-# 2. 정확도 계산
-#accuracy = accuracy_score(true_labels, pred_labels)
-#print(f"Accuracy: {accuracy:.4f}")
-
-# 3. 분류 보고서 출력
-#report = classification_report(true_labels, pred_labels, target_names=["Negative", "Positive"])
-#print("Classification Report:\n", report)
-
